b0616a4f0b32edc8be193f4644804a1ef77f8656
atoato88 <atoato88@gmail.com>
2017-09-05 15:44:57 +0900

    Change generate config logic from hardcode to copy file

--- setup-env.sh
+++ setup-env.sh
@@ -11,22 +11,7 @@ fi
 setup-tmux() {
 	mkdir -p ~/.tmux/plugins
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-	cat << EOF > ~/.tmux.conf
-# List of plugins
-set -g @plugin 'tmux-plugins/tpm'
-set -g @plugin 'tmux-plugins/tmux-sensible'
-
-# Other examples:
-# set -g @plugin 'github_username/plugin_name'
-# set -g @plugin 'git@github.com/user/plugin'
-# set -g @plugin 'git@bitbucket.com/user/plugin'
-set -g @plugin 'tmux-plugins/tmux-logging'
-
-# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
-run '~/.tmux/plugins/tpm/tpm'
-EOF
-
+	cp -a ./conf/.tmux.conf ~/
 	~/.tmux/plugins/tpm/bin/install_plugins
 }
 
