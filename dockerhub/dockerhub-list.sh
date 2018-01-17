8e6bcfb97c7ddc38746fd1cd5bf1067f0fb432dd
atoato88 <atoato88@gmail.com>
2018-01-17 10:29:39 +0900

    Use environment values for authentication.

--- dockerhub/dockerhub-list.sh
+++ dockerhub/dockerhub-list.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Original script is https://gist.github.com/kizbitz/175be06d0fbbb39bc9bfa6c0cb0d4721 
+
 # Example for the Docker Hub V2 API
 # Returns all images and tags associated with a Docker Hub organization account.
 # Requires 'jq': https://stedolan.github.io/jq/
 
 # set username, password, and organization
-UNAME=""
-UPASS=""
-ORG=""
-PAGE_SIZE=100
+UNAME=$UNAME
+UPASS=$UPASS
+ORG=$ORG
+PAGE_SIZE=${PAGE_SIZE:-100}
 
 function show_images() {
   page=$1
@@ -56,22 +58,4 @@ do
   #echo loop $i/$LOOP_COUNT
   show_images $i $PAGE_SIZE
 done
-
-#REPO_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORG}/?page_size=100 | jq -r '.results|.[]|.name')
-#
-## output images & tags
-#echo
-#echo "Images and tags for organization: ${ORG}"
-#echo
-#for i in ${REPO_LIST}
-#do
-#  echo "${i}"
-#  # tags
-#  #IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORG}/${i}/tags/?page_size=100 | jq -r '.results|.[]|.name')
-#  #for j in ${IMAGE_TAGS}
-#  #do
-#  #  echo "  - ${j}"
-#  #done
-#  #echo
-#done
 
