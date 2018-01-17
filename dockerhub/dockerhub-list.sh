aeb613eeee64ce26e06d12656164c5c6a2a6aaad
atoato88 <atoato88@gmail.com>
2018-01-17 10:25:00 +0900

    Add script to list images in org from hub.docker.com.

---
+++ dockerhub/dockerhub-list.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Example for the Docker Hub V2 API
+# Returns all images and tags associated with a Docker Hub organization account.
+# Requires 'jq': https://stedolan.github.io/jq/
+
+# set username, password, and organization
+UNAME=""
+UPASS=""
+ORG=""
+PAGE_SIZE=100
+
+function show_images() {
+  page=$1
+  page_size=$2
+	REPO_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORG}/?page=$page\&page_size=$page_size | jq -r '.results|.[]|.name')
+	for i in ${REPO_LIST}
+	do
+		echo "${i}"
+		# tags
+		#IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORG}/${i}/tags/?page_size=100 | jq -r '.results|.[]|.name')
+		#for j in ${IMAGE_TAGS}
+		#do
+		#  echo "  - ${j}"
+		#done
+		#echo
+	done
+}
+
+# -------
+
+set -e
+#echo
+
+# get token
+#echo "Retrieving token ..."
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${UNAME}'", "password": "'${UPASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+#echo token $TOKEN
+
+IMAGE_COUNT=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORG}/? | jq -r .count)
+#echo image count $IMAGE_COUNT
+
+LOOP_COUNT=$(expr $IMAGE_COUNT / $PAGE_SIZE)
+#echo loop count $LOOP_COUNT
+
+if $(expr $IMAGE_COUNT % $PAGE_SIZE > /dev/null)
+then
+  LOOP_COUNT=$(expr $LOOP_COUNT + 1)
+fi
+
+# get list of repositories
+#echo "Retrieving repository list ..."
+
+for i in $(seq $LOOP_COUNT)
+do
+  #echo loop $i/$LOOP_COUNT
+  show_images $i $PAGE_SIZE
+done
+
+#REPO_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORG}/?page_size=100 | jq -r '.results|.[]|.name')
+#
+## output images & tags
+#echo
+#echo "Images and tags for organization: ${ORG}"
+#echo
+#for i in ${REPO_LIST}
+#do
+#  echo "${i}"
+#  # tags
+#  #IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORG}/${i}/tags/?page_size=100 | jq -r '.results|.[]|.name')
+#  #for j in ${IMAGE_TAGS}
+#  #do
+#  #  echo "  - ${j}"
+#  #done
+#  #echo
+#done
+
