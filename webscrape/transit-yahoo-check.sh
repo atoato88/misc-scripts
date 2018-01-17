100512c5afa1da128b0578495d05e6b23c5bc767
atoato88 <atoato88@gmail.com>
2018-01-17 11:20:13 +0900

    Add check script to transit.yahoo.co.jp.

---
+++ webscrape/transit-yahoo-check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Reference
+# https://stackoverflow.com/questions/2220301/how-to-evaluate-http-response-codes-from-bash-shell-script
+# https://orebibou.com/2015/09/%E3%82%B7%E3%82%A7%E3%83%AB%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88%E3%81%A7%E3%80%81%E9%85%8D%E5%88%97%E5%86%85%E3%81%AE%E6%96%87%E5%AD%97%E5%88%97%E3%81%A8%E4%B8%80%E8%87%B4%E3%81%97%E3%81%AA/
+
+
+
+set -eu
+set -x
+
+URL="https://transit.yahoo.co.jp"
+
+function is_http_error() {
+  local error_code_array=(404 )
+  local success_code_array=(200 )
+  local code=$1
+
+  if `echo ${success_code_array[@]} | grep -q "${code}"`
+  then
+    echo 0
+    return 0
+  else
+    echo 1
+    return 1
+  fi
+}
+
+
+# Check url response code.
+RESPONSE=$(curl --write-out %{http_code} --silent --output /dev/null ${URL})
+echo ${RESPONSE}
+
+echo $(is_http_error ${RESPONSE})
+
+# Check target xpath. 
+
+exit 0
