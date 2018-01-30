edea598f3e4a4a5f009460a1e829f5dc348d9e87
atoato88 <atoato88@gmail.com>
2018-01-30 13:55:07 +0900

    Update quickstart.sh.

--- tripleo/auto-quickstart.sh
+++ tripleo/auto-quickstart.sh
@@ -8,6 +8,11 @@ function print_separation() {
 #set -eu
 set -x
 
+sudo setenforce 0
+sudo yum groupinstall "Virtualization Host" -y
+sudo yum install git lvm2 lvm2-devel -y
+ssh root@$VIRTHOST uname -a
+
 HOME="/home/hid-nakamura"
 #CONFIG="config/general_config/featureset032.yml"
 CONFIG=""
@@ -18,14 +23,61 @@ then
   PARAM_CONFIG0="--config .quickstart/${CONFIG}" 
   PARAM_CONFIG="--config ${CONFIG}" 
 fi
-NODE="config/nodes/3ctlr_1comp.yml"
-#NODE=""
+#NODE="config/nodes/3ctlr_1comp.yml"
+NODE=""
 PARAM_NODE=""
 if [[ -n ${NODE} ]]
 then
   PARAM_NODE="--nodes ${NODE}" 
 fi
 PARAM_EXTRA="--extra-vars deploy_timeout=120"
+
+VIRTHOST=127.0.0.2
+CONFIG=~/deploy-config.yaml
+
+cat > $CONFIG << EOF
+control_memory: 6144
+compute_memory: 6144
+
+undercloud_memory: 8192
+
+undercloud_vcpu: 4
+
+default_vcpu: 1                                                                                                    
+
+node_count: 4                                                                                                      
+  
+overcloud_nodes:                                                                                                   
+  - name: control_0
+    flavor: control                                                                                                
+    virtualbmc_port: 6230                                                                                          
+
+  - name: control_1                                                                                                
+    flavor: control
+    virtualbmc_port: 6231                                                                                          
+                                                                                                                   
+  - name: control_2                                                                                                
+    flavor: control
+    virtualbmc_port: 6232
+
+  - name: compute_0                                                                                                
+    flavor: compute                                                                                                
+    virtualbmc_port: 6233                                                                                          
+  
+topology: >-
+  --compute-scale 1
+  --control-scale 3
+                                                                                                                   
+containerized_overcloud: true                                                                                      
+delete_docker_cache: true
+enable_pacemaker: true
+run_tempest: false
+extra_args: >-
+  --libvirt-type qemu
+  --ntp-server pool.ntp.org                                                                                        
+  -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml
+  -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml                                       
+EOF
 
 
 print_separation "clean env"
@@ -36,7 +88,8 @@ rm -rf ~/.quickstart
 
 print_separation "init undercloud vm"
 bash quickstart.sh --install-deps
-bash quickstart.sh -R master --teardown all --tags all ${PARAM_CONFIG0} ${PARAM_EXTRA} -p quickstart.yml 127.0.0.2
+#bash quickstart.sh -R master --teardown all --tags all ${PARAM_CONFIG0} ${PARAM_EXTRA} -p quickstart.yml 127.0.0.2
+bash quickstart.sh --release master --teardown all --tags all ${PARAM_CONFIG0} ${PARAM_EXTRA} -e @$CONFIG -p quickstart.yml $VIRTHOST 
 if [[ $? -gt 0 ]]
 then
   echo NG retry
@@ -47,47 +100,33 @@ fi
 
 cd .quickstart
 
-#sudo sed -i '/composable_roles:/adeploy_timeout:\ 180' config/general_config/featureset032.yml
-
-echo '# Featureset for deploying a 2 controller setup using the deployed server
-# method
-composable_services: false
-    
-network_isolation: false
-enable_pacemaker: true
-overcloud_ipv6: false
-containerized_overcloud: true
-    
-# This enables TLS for the undercloud which will also make haproxy bind to the
-# configured public-vip and admin-vip.
-undercloud_generate_service_certificate: false
-
-# This enables the deployment of the overcloud with SSL.
-ssl_overcloud: false
-' >> ${NODE}
-
 print_separation "install undercloud"
-bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-undercloud.yml 127.0.0.2
+#bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-undercloud.yml 127.0.0.2
+bash ./tripleo-quickstart/quickstart.sh --release master --no-clone --teardown none --tags all ${PARAM_CONFIG} ${PARAM_EXTRA} -I -e @$CONFIG -p quickstart-extras-undercloud.yml $VIRTHOST 
 
 print_separation "overcloud prep"
 ssh -F ~/.quickstart/ssh.config.ansible undercloud -- sudo sed -i '/#stack_action_timeout/a\ stack_action_timeout\ =\ 36000' /etc/heat/heat.conf
 ssh -F ~/.quickstart/ssh.config.ansible undercloud -- sudo systemctl restart openstack-heat-engine
 ssh -F ~/.quickstart/ssh.config.ansible undercloud -- "date; sudo grep 36000 /var/log/heat/heat-engine.log"
 
-bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-overcloud-prep.yml 127.0.0.2
+#bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-overcloud-prep.yml 127.0.0.2
+bash ./tripleo-quickstart/quickstart.sh --release master --no-clone --teardown none --tags all ${PARAM_CONFIG} ${PARAM_EXTRA} -I -e @$CONFIG -p quickstart-extras-overcloud-prep.yml $VIRTHOST 
 if [[ $? -gt 0 ]]
 then
   echo NG retry
-  bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-overcloud-prep.yml 127.0.0.2
+  #bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-overcloud-prep.yml 127.0.0.2
+  bash ./tripleo-quickstart/quickstart.sh --release master --no-clone --teardown none --tags all ${PARAM_CONFIG} ${PARAM_EXTRA} -I -e @$CONFIG -p quickstart-extras-overcloud-prep.yml $VIRTHOST 
 fi
 
 print_separation "overcloud install"
 ssh -F ~/.quickstart/ssh.config.ansible undercloud -- sudo sed -i "/name:\ ControllerApi/a'\ \ disable_upgrade_deployment:\ True'" /usr/share/openstack-tripleo-heat-templates/ci/environments/multinode-3nodes.yaml
 
-bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-overcloud.yml 127.0.0.2
+#bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-overcloud.yml 127.0.0.2
+bash ./tripleo-quickstart/quickstart.sh --release master --no-clone --teardown none --tags all ${PARAM_CONFIG} ${PARAM_EXTRA} -I -e @$CONFIG -p quickstart-extras-overcloud.yml $VIRTHOST 
 
 print_separation "overcloud validation"
-bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-validate.yml 127.0.0.2
+#bash ./tripleo-quickstart/quickstart.sh -R master --no-clone --tags all ${PARAM_NODE} ${PARAM_CONFIG} ${PARAM_EXTRA} -I --teardown none -p quickstart-extras-validate.yml 127.0.0.2
+bash ./tripleo-quickstart/quickstart.sh --release master --no-clone --teardown none --tags all ${PARAM_CONFIG} ${PARAM_EXTRA} -I -e @$CONFIG -p quickstart-extras-validate.yml $VIRTHOST 
 
 exit 0
 
