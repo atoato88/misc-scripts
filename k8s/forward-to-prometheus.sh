738bceb9d80290ad0bdd173415c60556dcfd55b3
atoato88 <atoato88@gmail.com>
2018-03-08 10:24:35 +0900

    Update script for forward to k8s pod

--- k8s/forward-to-prometheus.sh
+++ k8s/forward-to-prometheus.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+# forward to pod on k8s
 
 TARGET_CONTEXT=${1:-"cluster"}
+NAMESPACE=${2:-"prometheus"}
+LOCAL_PORT=${4:-8090}
+REMOTE_PORT=${5:-9090}
 
-export POD_NAME=$(kubectl --context ${TARGET_CONTEXT} get pods -n prometheus | grep prometheus-server | cut -d ' ' -f 1)
+if [[ -z ${3} ]]
+then
+  export POD_NAME=$(kubectl --context ${TARGET_CONTEXT} get pods -n ${NAMESPACE} | grep prometheus-server | cut -d ' ' -f 1)
+else
+  export POD_NAME=${3}
+fi
+
 echo ${POD_NAME}
 
 while :
 do 
-  kubectl --context ${TARGET_CONTEXT} --namespace prometheus port-forward ${POD_NAME} 8090:9090
+  kubectl --context ${TARGET_CONTEXT} --namespace ${NAMESPACE} port-forward ${POD_NAME} ${LOCAL_PORT}:${REMOTE_PORT}
   date
   echo
 done
