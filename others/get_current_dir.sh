e3cfad39b10b297c737364463318feb89ca47b5b
atoato88 <atoato88@gmail.com>
2018-06-01 08:58:30 +0900

    Add get current dir script

---
+++ others/get_current_dir.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ref: https://qiita.com/t_cyrill/items/eb010de0fa40924bf31f
+
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+PARENT_DIR=$(cd $(dirname $0);cd ..;pwd)
+
+echo ${SCRIPT_DIR}
+echo ${PARENT_DIR}
