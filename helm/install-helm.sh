1a5ae138c3bac4656de08d04bfb2243eb9825224
atoato88 <atoato88@gmail.com>
2019-02-01 13:18:46 +0900

    Update shell for installing helm

--- helm/install-helm.sh
+++ helm/install-helm.sh
@@ -1,50 +1,10 @@
 #!/bin/bash
 
-set -x
-
-## install ##
-
-#cd ${HOME}
-curl -O https://storage.googleapis.com/kubernetes-helm/helm-v2.8.0-linux-amd64.tar.gz
-
-tar -zxvf helm-v2.8.0-linux-amd64.tar.gz
-
-mv -f linux-amd64/helm /usr/local/bin/helm
-
-helm help
-
-
-## init ##
-
-kubectl create serviceaccount tiller --namespace kube-system
-cat <<EOF > rbac-config.yaml
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: tiller
-  namespace: kube-system
----
-apiVersion: rbac.authorization.k8s.io/v1beta1
-kind: ClusterRoleBinding
-metadata:
-  name: tiller
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-  - kind: ServiceAccount
-    name: tiller
-    namespace: kube-system
-EOF
-
-kubectl create -f rbac-config.yaml
-
-helm init --service-account tiller
-
-kubectl get pods -n kube-system
-
-echo "run: helm repo update"
-
-
-exit 0
+export KUBECONFIG=/etc/kubernetes/admin.conf
+curl -L -O https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-linux-amd64.tar.gz
+tar -zxvf helm-v2.11.0-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
+helm init
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
