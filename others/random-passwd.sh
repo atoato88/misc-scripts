e4aa69f4889052365c9bdabe7aee08ee3e7e9e27
atoato88 <atoato88@gmail.com>
2020-12-02 21:46:12 +0900

    add mode for generate alnum in random-passwd.sh

--- others/random-passwd.sh
+++ others/random-passwd.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # ref: https://www.howtogeek.com/howto/30184/10-ways-to-generate-a-random-password-from-the-command-line/
 
+PATTERN_ALL="!@:;_A-Z-a-z-0-9"
+PATTERN_ALNUM="A-Za-z0-9"
 DIGIT=${1:-16}
+PARAM=${2:-"all"}
 
-< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${DIGIT};echo;
+if [[ ${PARAM} = "alnum" ]]
+then
+	< /dev/urandom tr -dc ${PATTERN_ALNUM} | head -c${DIGIT};echo;
+else
+	< /dev/urandom tr -dc ${PATTERN_ALL} | head -c${DIGIT};echo;
+fi
+
 
 
